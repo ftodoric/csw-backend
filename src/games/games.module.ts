b8f1e9c25d114bce9e194d1eeeca0a1e3b106820
ftodoric <fptodoric@gmail.com>
2023-05-20 14:48:08 +0200

    Remove typeorm module imports of for other modules from Game

--- src/games/games.module.ts
+++ src/games/games.module.ts
@@ -2,11 +2,8 @@ import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 
 import { AuthModule } from '@auth'
-import { User } from '@auth/entities'
 import { PlayersModule } from '@players'
-import { Player } from '@players/entities'
 import { TeamsModule } from '@teams'
-import { Team } from '@teams/entities'
 
 import { Game } from './entities'
 import { GamesController } from './games.controller'
@@ -17,15 +14,7 @@ import { TimerGateway } from './timer.gateway'
 @Module({
   controllers: [GamesController],
   providers: [GamesService, GamesRepository, TimerGateway],
-  imports: [
-    AuthModule,
-    TeamsModule,
-    PlayersModule,
-    TypeOrmModule.forFeature([User]),
-    TypeOrmModule.forFeature([Player]),
-    TypeOrmModule.forFeature([Team]),
-    TypeOrmModule.forFeature([Game]),
-  ],
+  imports: [AuthModule, TypeOrmModule.forFeature([Game]), TeamsModule, PlayersModule],
   exports: [GamesService],
 })
 export class GamesModule {}
